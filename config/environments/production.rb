b031407fe28b7570451573234977ccd5265c693e
Colby Schneider <ColbySchneider@gmail.com>
2013-03-20 19:16:01 -0400

    Change path for Production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,5 +66,5 @@ Omrails::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
   
 
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'http://aqueous-lake-9078.herokuapp.com' }
 end
